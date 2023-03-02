8dbb11904da06347cf7bada3ecbdc96030f410ea
Sonu Gupta <rg3029926@gmail.com>
2023-03-02 15:46:46 +0530

    My Portfolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import AllRoutes from './Routes/AllRoutes';
-import { Box } from '@chakra-ui/react';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box  className="App" id="home" fontFamily={"Titillium Web"} pt={12} bg="rgb(34,39,51)">
-   <Navbar/>
-   <AllRoutes/>
+    <Box
+      className="App"
+      id="home"
+      fontFamily={"Titillium Web"}
+      pt={12}
+      bg="rgb(34,39,51)"
+    >
+      <Navbar />
+      <AllRoutes />
     </Box>
   );
 }
